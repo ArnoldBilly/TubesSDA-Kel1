c6fba407122ca945f1abf5e9db477b4c0369ef25
ArnoldBilly <aarnoldbilly1907@gmail.com>
2025-06-04 15:50:46 +0700

    Adding tree & tree.txt

--- maintest.c
+++ maintest.c
@@ -4,51 +4,63 @@ int main() {
     srand(time(NULL));
     int pilih;
     char namaUNIV[50], pulau[15];
+    Address rootjawa, rootsumatera, rootkalimantan, rootsulawesi, rootpapua;
+    loadAtauInit(&rootjawa, &rootsumatera, &rootkalimantan, &rootsulawesi, &rootpapua);
+
     printf("Pilih pulau:\n");
     printf("1. Jawa\n2. Sumatera\n3. Kalimantan\n4. Sulawesi\n5. Papua\n");
-    printf("Masukkan pilihan: ");
+    printf("Masukkan pilihan (1-5): ");
     scanf("%d", &pilih);
+    getchar();
 
     pilih--;
     Address root = NULL;
     switch (pilih) {
-        case 0: 
-        root = initTreeJawa(); 
-        strcpy(pulau, "Jawa");
-        printTree(root, 0);
-        break;
-        case 1: 
-        root = initTreeSumatera();
-        strcpy(pulau, "Sumatera");
-        printTree(root, 0);
-        break;
-        case 2: 
-        root = initTreeKalimantan();
-        strcpy(pulau, "Kalimantan");
-        printTree(root, 0);
-        break;
-        case 3: 
-        root = initTreeSulawesi();
-        strcpy(pulau, "Sulawesi");
-        printTree(root, 0);
-        break;
-        case 4: 
-        root = initTreePapua();
-        strcpy(pulau, "Papua");
-        printTree(root, 0);
-        break;
+        case 0: root = rootjawa; strcpy(pulau, "Jawa"); break;
+        case 1: root = rootsumatera; strcpy(pulau, "Sumatera"); break;
+        case 2: root = rootkalimantan; strcpy(pulau, "Kalimantan"); break;
+        case 3: root = rootsulawesi; strcpy(pulau, "Sulawesi"); break;
+        case 4: root = rootpapua; strcpy(pulau, "Papua"); break;
         default: printf("Pilihan tidak valid\n"); return 1;
     }
-    getchar();
-    while (1)
-    {
+
+    printTree(root, 0);
+
     printf("\nDaftar Universitas yang ada di pulau %s:\n", pulau);
     showTree(root, 0);
-    printf("Masukkan nama universitas tempat ujian: ");
+    printf("Masukkan nama universitas tempat ujian (Pastikan sesuai dengan daftar diatas): ");
     fgets(namaUNIV, sizeof(namaUNIV), stdin);
     namaUNIV[strcspn(namaUNIV, "\n")] = '\0';
-    daftarUniv(root, namaUNIV);
-    printf("\n");
-    printTree(root, 0);
+    Address hasil = daftarUniv(root, namaUNIV);
+
+    if (hasil == NULL || hasil->jumlahPeserta > hasil->kapasitas) {
+        char jawaban[4];
+        printf("Apakah kamu ingin mendaftar ke salah satu rekomendasi? (Ya/Tidak): ");
+        fgets(jawaban, sizeof(jawaban), stdin);
+        jawaban[strcspn(jawaban, "\n")] = '\0';
+        if (strcmp(jawaban, "ya") == 0 || strcmp(jawaban, "Ya") == 0) {
+            printf("Masukkan nama universitas dari rekomendasi: ");
+            fgets(namaUNIV, sizeof(namaUNIV), stdin);
+            namaUNIV[strcspn(namaUNIV, "\n")] = '\0';
+            Address daftarUlang = findUniv(root, namaUNIV);
+            if (daftarUlang && daftarUlang->jumlahPeserta < daftarUlang->kapasitas) {
+                daftarUlang->jumlahPeserta++;
+                printf("Kamu berhasil mendaftar di %s\n", daftarUlang->namaUniv);
+                printf("Silahkan lanjut ke tahap selanjutnya\n");
+            }
+        } else {
+            printf("Silakan pilih universitas lain.\n");
+        }
     }
+
+    FILE *f = fopen("datauniv.txt", "w");
+    fprintf(f, "#JAWA\n"); simpanTreeKeFile(rootjawa, f, 0);
+    fprintf(f, "#SUMATERA\n"); simpanTreeKeFile(rootsumatera, f, 0);
+    fprintf(f, "#KALIMANTAN\n"); simpanTreeKeFile(rootkalimantan, f, 0);
+    fprintf(f, "#SULAWESI\n"); simpanTreeKeFile(rootsulawesi, f, 0);
+    fprintf(f, "#PAPUA\n"); simpanTreeKeFile(rootpapua, f, 0);
+    fclose(f);
+
+    printf("\nStatus Pendaftaran:\n");
+    printTree(root, 0);
 }
--- test.h
+++ test.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <windows.h>
+#define MAX_LINE 256
 
 typedef struct TreeNode* Address;
 
@@ -12,19 +14,26 @@ typedef struct TreeNode {
     char namaUniv[100];
     int kapasitas;
     int jumlahPeserta;
+    Address parent;
     Address firstChild;
     Address nextSibling;
+    Address prevSibling;
 } TreeNode;
 
 Address newNode(const char* nama);
 void addChild(Address parent, Address child);
+void simpanTreeKeFile(Address root, FILE *f, int level);
+Address bacaTreeDariFile(FILE *f);
+void loadAtauInit(Address *j, Address *s, Address *k, Address *u, Address *p);
 void printTree(Address root, int level);
 void showTree(Address root, int level);
 Address findUniv(Address root, const char* target);
 Address daftarUniv(Address root, const char* target);
+void rekomendasiAlternatif(Address root, const char* target);
 Address initTreeJawa();
 Address initTreeSumatera();
 Address initTreeKalimantan();
 Address initTreeSulawesi();
 Address initTreePapua();
+
 #endif
